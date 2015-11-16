0d3c1532aa0a85a48b5958f137fccba00d765307
Jordan Degner <jdegner0129@gmail.com>
2015-11-15 22:46:32 -0600

    Create item view layout and implement RecyclerView adapter

--- HudlU/app/src/main/java/degner/jordan/hudlu/MyAdapter.java
+++ HudlU/app/src/main/java/degner/jordan/hudlu/MyAdapter.java
@@ -2,8 +2,10 @@ package degner.jordan.hudlu;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Jordan on 11/15/15.
@@ -11,10 +13,14 @@ import android.view.ViewGroup;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private String[] mDataset;
 
+
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView mItemTextView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+
+            mItemTextView = (TextView) itemView.findViewById(R.id.item_text_view);
         }
     }
 
@@ -24,16 +30,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_item_view, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.mItemTextView.setText(mDataset[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataset.length;
     }
 }
