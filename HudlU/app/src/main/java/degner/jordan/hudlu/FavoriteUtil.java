2910a2551b24f90a227cba8f2f3289fa3b38bc97
Jordan Degner <jdegner0129@gmail.com>
2015-12-01 00:40:15 -0600

    Add FavoriteUtil skeleton

---
+++ HudlU/app/src/main/java/degner/jordan/hudlu/FavoriteUtil.java
@@ -0,0 +1,28 @@
+package degner.jordan.hudlu;
+
+import android.content.Context;
+
+import degner.jordan.hudlu.models.Favorite;
+import degner.jordan.hudlu.models.MashableNewsItem;
+import io.realm.RealmResults;
+
+/**
+ * Created by Jordan on 12/1/15.
+ */
+public class FavoriteUtil {
+    public static void addFavorite(Context context, MashableNewsItem newsItem) {
+        return;
+    }
+
+    public static void removeFavorite(Context context, MashableNewsItem newsItem) {
+        return;
+    }
+
+    public static boolean isFavorite(Context context, MashableNewsItem newsItem) {
+        return false;
+    }
+
+    public static RealmResults<Favorite> getAllFavorites(Context context) {
+        return null;
+    }
+}
